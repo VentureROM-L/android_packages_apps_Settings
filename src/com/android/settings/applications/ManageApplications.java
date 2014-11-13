3eb572597cbb86475bd5242f0f5e5428310ccea5
Adnan <adnan@cyngn.com>
2014-11-12 17:14:28 -0800

    Settings: Don't show protected apps options in restricted profiles.
    Change-Id: I38c2e8fd3508d360f0e23703ce6c939f02f3714e
    JIRA: 4668
    Issue: https://jira.cyanogenmod.org/browse/CYAN-4668

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -1113,8 +1113,10 @@ public class ManageApplications extends Fragment implements
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         menu.add(0, APP_INSTALL_LOCATION, 4, R.string.app_install_location_title)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-        menu.add(0, SHOW_PROTECTED_APPS, 5, R.string.protected_apps)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        if (!Utils.isRestrictedProfile(getActivity())) {
+            menu.add(0, SHOW_PROTECTED_APPS, 5, R.string.protected_apps)
+                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        }
         updateOptionsMenu();
     }
     
@@ -1152,7 +1154,9 @@ public class ManageApplications extends Fragment implements
             mOptionsMenu.findItem(SHOW_RUNNING_SERVICES).setVisible(showingBackground);
             mOptionsMenu.findItem(SHOW_BACKGROUND_PROCESSES).setVisible(!showingBackground);
             mOptionsMenu.findItem(RESET_APP_PREFERENCES).setVisible(false);
-            mOptionsMenu.findItem(SHOW_PROTECTED_APPS).setVisible(true);
+            if (!Utils.isRestrictedProfile(getActivity())) {
+                mOptionsMenu.findItem(SHOW_PROTECTED_APPS).setVisible(true);
+            }
             mShowBackground = showingBackground;
         } else {
             mOptionsMenu.findItem(SORT_ORDER_ALPHA).setVisible(mSortOrder != SORT_ORDER_ALPHA);
@@ -1160,7 +1164,9 @@ public class ManageApplications extends Fragment implements
             mOptionsMenu.findItem(SHOW_RUNNING_SERVICES).setVisible(false);
             mOptionsMenu.findItem(SHOW_BACKGROUND_PROCESSES).setVisible(false);
             mOptionsMenu.findItem(RESET_APP_PREFERENCES).setVisible(true);
-            mOptionsMenu.findItem(SHOW_PROTECTED_APPS).setVisible(true);
+            if (!Utils.isRestrictedProfile(getActivity())) {
+                mOptionsMenu.findItem(SHOW_PROTECTED_APPS).setVisible(true);
+            }
         }
     }
 
