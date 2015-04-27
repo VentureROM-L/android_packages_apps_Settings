SimpleAOSP-Lollipop/platform_packages_apps_Settings

status bar: native battery percentage
Thanks Rascarlo

Conflicts:
	res/values/simpleaosp_strings.xml

src/com/android/settings/simpleaosp/StatusBarSettings.java
@@ -1,0 +1,64 @@
+
+package com.android.settings.simpleaosp;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class StatusBarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    // General
+    private static String STATUS_BAR_GENERAL_CATEGORY = "status_bar_general_category";
+    // Native battery percentage
+    private static final String STATUS_BAR_NATIVE_BATTERY_PERCENTAGE = "status_bar_native_battery_percentage";
+
+    // General
+    private PreferenceCategory mStatusBarGeneralCategory;
+    // Native battery percentage
+    private CheckBoxPreference mStatusBarNativeBatteryPercentage;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.status_bar_settings);
+
+        // General category
+        mStatusBarGeneralCategory = (PreferenceCategory) findPreference(STATUS_BAR_GENERAL_CATEGORY);
+
+        // Native battery percentage
+        mStatusBarNativeBatteryPercentage = (CheckBoxPreference) getPreferenceScreen()
+                .findPreference(STATUS_BAR_NATIVE_BATTERY_PERCENTAGE);
+        mStatusBarNativeBatteryPercentage.setChecked((Settings.System.getInt(getActivity()
+                .getApplicationContext().getContentResolver(),
+                Settings.System.STATUS_BAR_NATIVE_BATTERY_PERCENTAGE, 0) == 1));
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        return false;
+    }
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
+       if (preference == mStatusBarNativeBatteryPercentage) {
+            value = mStatusBarNativeBatteryPercentage.isChecked();
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.STATUS_BAR_NATIVE_BATTERY_PERCENTAGE, value ? 1 : 0);
+            return true;
+        }
+ 		return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+}
+

VentureROM-L/android_packages_apps_Settings

	res/values/strings.xml
	res/values/venture_strings.xml
	res/xml/display_settings.xml

src/com/android/settings/venture/StatusBarSettings.java
@@ -1,0 +1,63 @@
+package com.android.settings.venture;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class StatusBarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    // General
+    private static String STATUS_BAR_GENERAL_CATEGORY = "status_bar_general_category";
+    // Native battery percentage
+    private static final String STATUS_BAR_NATIVE_BATTERY_PERCENTAGE = "status_bar_native_battery_percentage";
+
+    // General
+    private PreferenceCategory mStatusBarGeneralCategory;
+    // Native battery percentage
+    private CheckBoxPreference mStatusBarNativeBatteryPercentage;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.status_bar_settings);
+
+        // General category
+        mStatusBarGeneralCategory = (PreferenceCategory) findPreference(STATUS_BAR_GENERAL_CATEGORY);
+
+        // Native battery percentage
+        mStatusBarNativeBatteryPercentage = (CheckBoxPreference) getPreferenceScreen()
+                .findPreference(STATUS_BAR_NATIVE_BATTERY_PERCENTAGE);
+        mStatusBarNativeBatteryPercentage.setChecked((Settings.System.getInt(getActivity()
+                .getApplicationContext().getContentResolver(),
+                Settings.System.STATUS_BAR_NATIVE_BATTERY_PERCENTAGE, 0) == 1));
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        return false;
+    }
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
+       if (preference == mStatusBarNativeBatteryPercentage) {
+            value = mStatusBarNativeBatteryPercentage.isChecked();
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.STATUS_BAR_NATIVE_BATTERY_PERCENTAGE, value ? 1 : 0);
+            return true;
+        }
+ 		return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+}
+