67c56955e12a25fd369ab86c15e9a7874250f646
Chintan Shah <cgintan1998@gmail.com>
2015-05-15 11:25:56 -0600

    Desperate fix
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- Android.mk
+++ Android.mk
@@ -30,12 +30,11 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-include frameworks/opt/setupwizard/navigationbar/common.mk
-
 LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
     --extra-packages android.support.v7.cardview
 
+include frameworks/opt/setupwizard/navigationbar/common.mk
 include $(BUILD_PACKAGE)
 
 # Use the following include to make our test apk.
